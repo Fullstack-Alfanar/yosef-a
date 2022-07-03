176e6d785dcabf95e222864cbae72525f5a1afcc
Yosef Awad <yosef.awad.1@gmail.com>
2022-07-04 00:26:23 +0300

    refactor: moved div style to scss file

--- src/pages/ShoePage.js
+++ src/pages/ShoePage.js
@@ -14,12 +14,8 @@ function ShoePage(props) {
     }, []);
 
     return <>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div className="shoesContainer" >
             {shoeList.length === 0 ? <h3>No Shoes Yet!!<br />Try To Add One.</h3> : shoeList.map((elem) => <Shoes name={elem.name} size={elem.size} price={elem.price} imgSrc={elem.imgSrc} />)}
-            {/* 
-     
-            <Shoes name="Sandals" size={20} price={"1000"} imgSrc={"https://ae01.alicdn.com/kf/H0ae62f276634413bbb2fcf52a2e6ca41r/Summer-Old-Man-Wearing-Men-s-Soil-Leather-Beach-Shoes-Breathable-Mesh-Eyelids-Sandals-Baotou-Non.jpg"} /> 
-            */}
         </div>
         <Link to="/addShoe" className="addLink">Add</Link>
     </>
