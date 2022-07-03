fb4d7d748ba2926e4151344b9a0f29824abfa545
Yosef Awad <yosef.awad.1@gmail.com>
2022-07-03 23:57:51 +0300

    feat: add new shoes, render saved shoes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,5 @@
 import { useRoutes } from "react-router-dom";
+import AddShoe from "../pages/AddShoe";
 import ShoePage from "../pages/ShoePage";
 function AppRouter() {
     return useRoutes([
@@ -9,7 +10,15 @@ function AppRouter() {
         {
             path: "/employees",
             // temporary
-            element: <><h1>Employee</h1></> 
+            element: <><h1>Employee</h1></>
+        },
+        {
+            path: "/addShoe",
+            element: <AddShoe />
+        },
+        {
+            path: "/addEmployee",
+            element: <><h1>AddEmployee</h1></>
         }
     ]);
 }
